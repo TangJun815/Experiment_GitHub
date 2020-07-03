e51725399a981577502b41189e5e990baeffd62c
day11up11 <day11up11@hotmail.com>
2020-07-03 17:20:07 +0800

    develop v1.0.2

--- SourceCode/ConsoleApp/Program.cs
+++ SourceCode/ConsoleApp/Program.cs
@@ -13,6 +13,8 @@ namespace ConsoleApp
             Console.WriteLine("fix v1.0.1");
             //fix v1.0.1 no2...
             Console.WriteLine("闯到鬼了，fix v1.0.1 第二次！");
+            //develop v1.0.2
+            Console.WriteLine("develop v1.0.2");
         }
     }
 }

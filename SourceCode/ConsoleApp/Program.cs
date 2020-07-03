5722f7608078a79c73b0fbf7ae53a33d16dfb58f
tangjun <tangjun815@live.com>
2020-07-03 16:49:45 +0800

    闯到鬼了，fix v1.0.1 第二次！

--- SourceCode/ConsoleApp/Program.cs
+++ SourceCode/ConsoleApp/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp
             Console.WriteLine("develop v1.0.1");
             //fix v1.0.1
             Console.WriteLine("fix v1.0.1");
+            //fix v1.0.1 no2...
+            Console.WriteLine("闯到鬼了，fix v1.0.1 第二次！");
         }
     }
 }

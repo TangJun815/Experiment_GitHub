9c6c70feb43e43cc53cc3c893bf6c88bc98c2a80
day11up11 <67783111+day11up11@users.noreply.github.com>
2020-07-03 23:01:08 +0800

    Update Program.cs
    测试第三方用户修改代码。

--- SourceCode/ConsoleApp/Program.cs
+++ SourceCode/ConsoleApp/Program.cs
@@ -13,6 +13,8 @@ namespace ConsoleApp
             Console.WriteLine("fix v1.0.1");
             //fix v1.0.1 no2...
             Console.WriteLine("闯到鬼了，fix v1.0.1 第二次！");
+            //modify by daydayup
+            //1234567890
         }
     }
 }

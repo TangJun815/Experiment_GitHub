417d75924f9a0a9ec06ba175225895504b9d583a
alas11 <alas_11@hotmail.com>
2021-09-17 16:30:06 +0800

    ok

--- SourceCode/ConsoleApp/Program.cs
+++ SourceCode/ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");2
             //develop v1.0.1
             Console.WriteLine("develop v1.0.1");
             //fix v1.0.1
